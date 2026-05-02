2683a350ca3443c458e341141dde0f58fcfd950a
Eduardo Padilha <padilhanasc@gmail.com>
2026-05-01 23:10:52 -0300

    fix: renamed AutheticationController to AuthenticationsController

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Controllers\HomeController;
-use App\Controllers\AuthenticationController;
+use App\Controllers\AuthenticationsController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
 
-Route::get('/login', [AuthenticationController::class, 'new'])->name('users.login');
-Route::post('/login', [AuthenticationController::class, 'authenticate'])->name('users.authenticate');
-Route::get('/logout', [AuthenticationController::class, 'destroy'])->name('users.logout');
+Route::get('/login', [AuthenticationsController::class, 'new'])->name('users.login');
+Route::post('/login', [AuthenticationsController::class, 'authenticate'])->name('users.authenticate');
+Route::get('/logout', [AuthenticationsController::class, 'destroy'])->name('users.logout');
 
 
